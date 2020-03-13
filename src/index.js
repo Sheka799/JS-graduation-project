0403b5733c0a0a5e4790fb64e29956ae657d08e6
Evgenii Krivolapov <sheka1991_91@mail.ru>
2020-03-13 12:52:17 +0300

    создал папку fixMenu

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import dropdownMenu from './modules/dropdownMenu';
 import modalWindow from './modules/modalWindow1';
 import modalWindowTwo from './modules/modalWindow2';
 import gift from './modules/gift';
+import fixMenu from './modules/fixMenu';
 
 dropdownMenu();
 modalWindow();
 modalWindowTwo();
 gift();
+fixMenu();
 
 
 
---
+++ src/modules/fixMenu.js
@@ -0,0 +1,19 @@
+const fixMenu = () => {
+  const topMenu = document.querySelector('.hidden-large'); // Инициализируем блок навигации
+  
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+export default fixMenu;
