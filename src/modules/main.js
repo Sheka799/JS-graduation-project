cb55fe8a4dadf9363fe8ffe0c6b20c01b9e6266f
Evgenii Krivolapov <sheka1991_91@mail.ru>
2020-03-14 11:45:45 +0300

    исправлена ошибка в названии класса|main.js

--- src/modules/main.js
+++ src/modules/main.js
@@ -3,8 +3,8 @@ const main = () => {
   closePopup = document.querySelector('.close-menu-btn img'),
   popupMenu = document.querySelector('.popup-menu');
 
-openPopup.classList.add('open-popup');
-closePopup.classList.add('close-popup');
+openPopup.classList.add('open-popup-btn');
+closePopup.classList.add('close-popup-btn');
 
 const link = document.querySelectorAll('li a');
  for (let i=0; i < link.length; i++) {
@@ -13,9 +13,9 @@ const link = document.querySelectorAll('li a');
 
   document.addEventListener('click', (event) => {
     const target = event.target; 
-    if (target.classList.contains('open-popup')) {
+    if (target.classList.contains('open-popup-btn')) {
       popupMenu.style.display = 'flex';
-    } else if (target.classList.contains('close-popup')) {
+    } else if (target.classList.contains('close-popup-btn')) {
       popupMenu.style.display = 'none';
     } else if (target.classList.contains('link')) {
       popupMenu.style.display = 'none';
